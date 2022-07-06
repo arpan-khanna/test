43f0cf533a51b0ed4d6b7e01e3b051f9bb17d003
Arpan <arpankhanna70@gmail.com>
2022-07-07 00:17:36 +0530

    second_commit

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def view_name(request):
-    return render(request, 'template_name.html', {})
+    return render(request, 'myapp.html', {})
